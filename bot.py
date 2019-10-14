6112b863277ea9fa61a661bce71b412097621767
DarkClassic <40740053+DarkClassic@users.noreply.github.com>
2019-10-15 00:23:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,16 +20,16 @@ print(bot.get_me())
 
 
 
-def log(message):
-    print("----------------")
-    file = open("LOG.txt", mode='a', encoding='utf_8')
-    file2 = open("username.txt", mode='a', encoding='utf_8')
-    f_str = str(str(message.chat.id) + "\n")
-    file2.write(str(message.from_user.username) + "\n")
-    file.write(f_str)
-    file.close()
-    file2.close()
-    print("Сообщение от {0} {1} {2} {3} TEXT - {4}. \n ".format(message.from_user.first_name,message.from_user.last_name,message.from_user.username,str(message.from_user.id),str(message.text)))
+#def log(message):
+    #print("----------------")
+   # file = open("LOG.txt", mode='a', encoding='utf_8')
+    #file2 = open("username.txt", mode='a', encoding='utf_8')
+    #f_str = str(str(message.chat.id) + "\n")
+  # file2.write(str(message.from_user.username) + "\n")
+  #  file.write(f_str)
+   # file.close()
+    #file2.close()
+   # print("Сообщение от {0} {1} {2} {3} TEXT - {4}. \n ".format(message.from_user.first_name,message.from_user.last_name,message.from_user.username,str(message.from_user.id),str(message.text)))
 
 
 
